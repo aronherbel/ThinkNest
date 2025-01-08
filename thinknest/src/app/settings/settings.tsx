731631225ab54193c6a072eb0efabcaceb00be54
ajnurademi <ajnur.ademi@hotmail.com>
2025-01-08 08:04:11 +0100

    added Feedback Form

--- thinknest/src/app/settings/settings.tsx
+++ thinknest/src/app/settings/settings.tsx
@@ -10,7 +10,7 @@ const Settings = () => {
           href="https://forms.office.com/e/fKEW4rxy1z?origin=lprLink" 
           target="_blank" 
           rel="noopener noreferrer"
-          className="px-5 py-2 text-lg text-white bg-green-600 rounded hover:bg-green-700"
+          className="px-4 py-2 text-ml text-white bg-black rounded hover:bg-green-700"
         >
           Give us your feedback
         </a>
