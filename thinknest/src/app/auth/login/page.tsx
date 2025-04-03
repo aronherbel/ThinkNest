c01b9f7a52606dcf65c490c5481ea0eccf323e89
efeborasaglam <efebora.saglam@hotmail.com>
2025-04-03 21:36:29 +0200

    fix(login, signup, calendar_active), calender icon fixed, login signup design fixed

--- thinknest/src/app/auth/login/page.tsx
+++ thinknest/src/app/auth/login/page.tsx
@@ -4,6 +4,8 @@ import { FormEvent, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/lib/firebase";
 import { useRouter } from "next/navigation";
+import { Image } from "lucide-react";
+import logo from "../../../../public/assets/img/thinknest_logo.png"
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -16,7 +18,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-     await signInWithEmailAndPassword(auth, email, password);
+      await signInWithEmailAndPassword(auth, email, password);
 
       router.push("/dashboard");
     } catch (err: any) {
@@ -26,42 +28,54 @@ export default function LoginPage() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
-        <h1 className="text-2xl font-bold text-center">Login</h1>
-        <form onSubmit={handleLogin} className="space-y-4">
-          <div>
-            <label className="block mb-1 text-gray-700">E-Mail</label>
-            <input
-              type="email"
-              placeholder="E-Mail"
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+      <div className="container mx-auto px-4">
+        <div className="row flex flex-wrap -mx-4 bg-[#28AD5E] shadow-md ">
+          <div className="col w-full md:w-1/2 px-4 relative bg-black">
+            <img src={logo.src} alt="Logo"/>
           </div>
-          <div>
-            <label className="block mb-1 text-gray-700">Passwort</label>
-            <input
-              type="password"
-              placeholder="Passwort"
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <div className="col w-full md:w-1/2 px-4">
+            <div className="mt-40 rounded-lg p-8 space-y-6 ">
+              <h1 className="text-2xl font-bold text-center">Login</h1>
+              <form onSubmit={handleLogin} className="space-y-4">
+                <div>
+                  <label className="block mb-1 text-gray-700">E-Mail</label>
+                  <input
+                    type="email"
+                    placeholder="E-Mail"
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label className="block mb-1 text-gray-700">Passwort</label>
+                  <input
+                    type="password"
+                    placeholder="Passwort"
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-black text-white py-2 px-4 rounded hover:bg-gray-700 transition-colors"
+                >
+                  Einloggen
+                </button>
+                <div className="text-center text-sm">
+                  <a
+                    href="/auth/signup"
+                    className="text-black hover:text-blue-600"
+                  >
+                    Don't have an account yet?
+                  </a>
+                </div>
+              </form>
+              {error && <p className="text-red-500 text-center">{error}</p>}
+            </div>
           </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors"
-          >
-            Einloggen
-          </button>
-          <div className="text-center text-sm">
-            <a href="/auth/signup" className="text-blue-500 hover:text-blue-600">
-              Don't have an account yet?
-            </a>
-          </div>
-        </form>
-        {error && <p className="text-red-500 text-center">{error}</p>}
+        </div>
       </div>
     </div>
   );
